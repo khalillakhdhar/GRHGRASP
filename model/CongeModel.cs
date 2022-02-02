3e3ba7d5e452badddc3ad2e090de8e74585a9821
khalil <khalillakhdharatc@gmail.com>
2022-02-02 15:27:03 +0100

    Initial commit

---
+++ model/CongeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRHGRASP.model
+{
+    class CongeModel
+    {
+        public int id { get; set; }
+        public string debut { get; set; }
+    }
+}
---
+++ model/SalaireModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRHGRASP.model
+{
+    class SalaireModel
+    {
+        public double Nette { get; set; }
+        public double Brute { get; set; }
+        public int idemp { get; set; }
+
+    }
+}
