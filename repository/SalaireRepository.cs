b839b58989d809de429fc906f45dad59d438a1bd
khalil <khalillakhdharatc@gmail.com>
2022-02-02 15:56:21 +0100

    Update SalaireRepository.cs

--- repository/SalaireRepository.cs
+++ repository/SalaireRepository.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GRHGRASP.model;
 
 namespace GRHGRASP.repository
 {
     interface SalaireRepository
     {
+        void createSalaire(SalaireModel salaire);
+        List<SalaireModel> GetSalaireModels();
     }
 }
